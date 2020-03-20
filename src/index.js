8221c00c52a2833cbacac7d1ab725fce060c6228
unknown <C:\Users\\AppData\Roaming\The Bat!>
2020-03-20 12:43:06 +0300

    add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let br2='';
+    let result = [];
+                       
+    for (let i=0; i<str.length; i++) {
+       for(let j=0;j<bracketsConfig.length;j++){
+          if (str[i] == bracketsConfig[j][0] && result[result.length-1] !== str[i]) {
+              result.push(str[i]);
+              break;
+      } else if (str[i] == bracketsConfig[j][1]) {
+        const br = result.pop();
+             
+  
+          if (br == bracketsConfig[j][0]){
+              br2 = bracketsConfig[j][1];
+              } else if (br == bracketsConfig[j][1]){
+                  br2 = bracketsConfig[j][0];
+              }
+  
+          if (br2 !== str[i]) { 
+            
+              return false; 
+          }
+          break;
+        }
+        
+      }
+    }
+  
+    
+    return result.length == 0; 
 }
